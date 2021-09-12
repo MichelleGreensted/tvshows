4cf1ceb7813931c60be0557d638cbbb2b516d6cf
MichelleG <michellegreensted@gmail.com>
2021-09-13 00:20:12 +0100

    Added live API fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
-// import { greet } from "./utils/greet";
 import React, { useState } from "react";
 import "./app.css";
-import episodes from "./episodes.json";
-// import EpisodeData from "./EpisodeData";
-import EpisodeCard from "./components/EpisodeCard";
+// import episodes from "./episodes.json";
+//components
+import EpisodeCard, { IEpisode } from "./components/EpisodeCard";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+//helper functions
 import doesEpisodeContainInNameOrSummary from "./utils/doesEpisodeContain";
 
 function App(): JSX.Element {
+  const [EpisodesData, setData] = useState<IEpisode[]>([]);
+  async function fetchEpisodeData() {
+    const response = await fetch("https://api.tvmaze.com/shows/82/episodes");
+    const jsonData = await response.json();
+    setData(jsonData);
+  }
+
   const [searchTerm, setSearchTerm] = useState("");
   function handleChange(event: {
     target: { value: React.SetStateAction<string> };
   }) {
     setSearchTerm(event.target.value);
   }
-  const matchingEpisodes = episodes.filter((episode) =>
+
+  const matchingEpisodes = EpisodesData.filter((episode) =>
     doesEpisodeContainInNameOrSummary(episode, searchTerm)
   );
+
   return (
     <>
       <header>
@@ -28,7 +37,7 @@ function App(): JSX.Element {
           <input onChange={handleChange} />
           <br />
           Displaying results for '{searchTerm}' in {matchingEpisodes.length}/
-          {episodes.length} episodes
+          {EpisodesData.length} episodes
         </div>
         <div className={"container"}>
           {matchingEpisodes.map((episode) => (
